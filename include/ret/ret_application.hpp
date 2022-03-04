260fda11a0bbe2947ea03fecc45a21fcc49a3faa
ipa-alb <aulon.bajrami@ipa.fraunhofer.de>
2022-03-04 14:57:53 +0100

    update button params and parametrize pre mash height

--- include/ret/ret_application.hpp
+++ include/ret/ret_application.hpp
@@ -15,7 +15,7 @@ public:
                  std::string robot,
                  std::string ip,
                  unsigned short port);
-  void LoggedButtonMash(MoveitCustomApi* moveit_api, geometry_msgs::Pose target_pose, double height, int button_no);
+  void LoggedButtonMash(MoveitCustomApi* moveit_api, geometry_msgs::Pose target_pose, int button_no);
   void LoadTargetPoses(ros::NodeHandle nh);
 
 private:
@@ -23,8 +23,9 @@ private:
   std::string prompts_;
   std::string robot_;
   EnduranceTestLogger logger_;
-  geometry_msgs::Pose target_pose1_, target_pose2_;
-  
+  geometry_msgs::Pose target_pose1_;
+  geometry_msgs::Pose target_pose2_;
+  double pre_mash_height_;
 };
 
 #endif // RET_APPLICATION_HPP
